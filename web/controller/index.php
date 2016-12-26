0f50903c7bf1e5447d82b7249eebaca3ae9a45a1
banpai <banpai@yeah.net>
2016-12-26 19:01:06 +0800

    添加iview

--- web/controller/index.php
+++ web/controller/index.php
@@ -5,15 +5,30 @@ $db = DB::get_db();
 if (Request::is_ajax()) {
   $action = Request::get(1);
   if ($action == 'code') {
+    //获取代码库内容
     $type = Request::post('type');
     $sql = "select * from code where type = ? order by updatetime asc";
     $parm = array($type);
     $res = $db->query($sql, $parm);
-    //获取代码库内容
     Response::json(array(
       'errcode' => '0', 
       'errmsg' => '',
       'data' => $res  
+    ));
+  }else if($action == 'level'){
+    $sql = "select * from level order by CAST(`li` AS DECIMAL) desc limit 1, 10";
+    $res = $db->query($sql);
+    $set = array();
+    foreach($res as $i => $v){
+        $set[$i]['id'] = $v['id'];
+        $set[$i]['li'] = $v['li'];
+        $set[$i]['state'] = $v['state'];
+    }
+    //获取等级
+    Response::json(array(
+      'errcode' => '0',
+      'errmsg' => '',
+      'data' => $set  
     ));
   }
 }
--- web/pub/js/index.js
+++ web/pub/js/index.js
@@ -25,21 +25,69 @@ var app = new Vue({
   data: {
     input: '# hello',
     tasksearch: '',
+    visible: false,
+    jilian: false,
     //模块的显示隐藏
     model: 'task',
+    codesearch: '',
     taskli: [{
       date: '2016-12-14',
       lidata: []
+    }],
+    data: [{
+      value: 'beijing',
+      label: '北京',
+      children: [{
+        value: 'gugong',
+        label: '故宫'
+      }, {
+        value: 'tiantan',
+        label: '天坛'
+      }, {
+        value: 'wangfujing',
+        label: '王府井'
+      }]
+    }, {
+      value: 'jiangsu',
+      label: '江苏',
+      children: [{
+        value: 'nanjing',
+        label: '南京',
+        children: [{
+          value: 'fuzimiao',
+          label: '夫子庙',
+        }]
+      }, {
+        value: 'suzhou',
+        label: '苏州',
+        children: [{
+          value: 'zhuozhengyuan',
+          label: '拙政园',
+        }, {
+          value: 'shizilin',
+          label: '狮子林',
+        }]
+      }],
     }]
   },
   computed: {
     compiledMarkdown: function () {
-      return marked(this.input, { sanitize: true })
+      return marked(this.input, {
+        sanitize: true
+      })
     }
   },
   methods: {
-    update: function(e){
+    update: function (e) {
       this.input = e.target.value;
+    },
+    change: function (status) {
+      // console.log(status);
+      this.jilian = status;
+      // this.$Message.info('开关状态：' + status);
+    },
+    show: function () {
+      this.visible = true;
     }
   }
 });
@@ -47,44 +95,47 @@ var app = new Vue({
 document.onkeydown = function (event) {
   var e = event || window.event || arguments.callee.caller.arguments[0];
   if (e && e.keyCode == 13) { // enter 键
-    console.log(app.tasksearch);
-    var sel = {
-      name: app.tasksearch
-    };
-    app.taskli[0].lidata.push(sel);
-    app.tasksearch = '';
+    if (app.model === 'task') {
+      console.log(app.tasksearch);
+      var sel = {
+        name: app.tasksearch
+      };
+      app.taskli[0].lidata.push(sel);
+      app.tasksearch = '';
+    }
   }
 };
 
 //统一管理接口
-var portsfun = (function(){
-    //获取按钮模块数据
-    var getbuttonsdata = function () {
-      ajax('/js/data/buttons.json', {}, function (data) {
-        right.buttons = data.data;
-      });
-    };
-    //获取等级数据
-    var getleveldata = function () {
-      ajax('/js/data/level.json', {}, function (data) {
-        left.level = data.data;
-      });
-    };
-    //获取代码库的代码
-    var getcode = function(){
-      
-      ajax('index-code.html', {
-        type: '2'
-      }, function(data){
-        var xx = JSON.stringify(data);
-        console.log(xx);
-      });
-    };
-    return {
-      getbuttonsdata: getbuttonsdata,
-      getleveldata: getleveldata,
-      getcode: getcode
-    };
+var portsfun = (function () {
+  //获取按钮模块数据
+  var getbuttonsdata = function () {
+    ajax('/js/data/buttons.json', {}, function (data) {
+      right.buttons = data.data;
+    });
+  };
+  //获取等级数据
+  var getleveldata = function () {
+    ajax('index-level.html', {}, function (data) {
+      left.level = data.data;
+      var xx = JSON.stringify(data);
+      console.log(xx);
+    });
+  };
+  //获取代码库的代码
+  var getcode = function () {
+    ajax('index-code.html', {
+      type: '2'
+    }, function (data) {
+      var xx = JSON.stringify(data);
+      console.log(xx);
+    });
+  };
+  return {
+    getbuttonsdata: getbuttonsdata,
+    getleveldata: getleveldata,
+    getcode: getcode
+  };
 }());
 
 //thinghigh初始化模块
